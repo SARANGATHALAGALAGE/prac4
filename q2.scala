5bc9273eb180379a7e70bfa0bee48e862609575b
SARANGATHALAGALAGE <140165039+SARANGATHALAGALAGE@users.noreply.github.com>
2024-07-12 11:38:35 +0530

    q2.scala

--- q2.scala
+++ q2.scala
@@ -1,17 +1,17 @@
-object NumberMatcher extends App {
-  if (args.length != 1) {
-    println("Please provide exactly one integer as input.")
-  } else {
-    try {
-      val input = args(0).toInt
-
-      input match {
-        case x if x <= 0 => println("Negative/Zero is input")
-        case x if x % 2 == 0 => println("Even number is given")
-        case _ => println("Odd number is given")
-      }
-    } catch {
-      case _: NumberFormatException => println("Please provide a valid integer.")
+object Main {
+  def main(args: Array[String]): Unit = {
+    print("Enter the Number : ")
+    var number: Int = scala.io.StdIn.readInt()
+    print("That number is ")
+    PatternMatching(number)
+  }
+  def PatternMatching(num: Int): Unit = {
+    if (num <= 0) {
+      print("Negative / Zero")
+    } else if (num % 2 == 0) {
+      print("Even number")
+    } else if (num % 2 != 0) {
+      print("Odd number")
     }
   }
 }
